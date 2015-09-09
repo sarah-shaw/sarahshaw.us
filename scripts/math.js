962e98d6c1b76a8ef5c7311521969152d07d935c
Sarah Shaw <s.antoinetteshaw@gmail.com>
2015-09-09 13:45:15 -0400

    Resume text added
    Added experience and skills to resume page

---
+++ scripts/math.js
@@ -0,0 +1,13 @@
+
+function getNumberX(min, max) {
+	return Math.floor(Math.random() * (11 - 1) + 1);s
+}
+
+function getNumberY(min, max) {
+   return Math.floor(Math.random() * (11 - 1) + 1);
+}
+
+prompt(getNumberX() + ' x ' + getNumberY() + ' = ');
+
+
+
